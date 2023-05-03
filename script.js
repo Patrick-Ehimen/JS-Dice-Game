c35382938ec8102fd23ee4be1613914c3ada1e22
Patrick-Ehimen <osepatrick.ehimen77@gmail.com>
2023-05-03 12:14:33 +0100

    Update script.js

--- script.js
+++ script.js
@@ -61,25 +61,27 @@ btnRoll.addEventListener("click", function () {
 });
 
 btnHold.addEventListener("click", function () {
-  // 1. Add current score to active player's score
-  scores[activePlayer] += currentScore;
+  if (playing) {
+    // 1. Add current score to active player's score
+    scores[activePlayer] += currentScore;
 
-  // scores[1] = scores[1] + currentScore
-  document.getElementById(`score--${activePlayer}`).textContent =
-    scores[activePlayer];
+    // scores[1] = scores[1] + currentScore
+    document.getElementById(`score--${activePlayer}`).textContent =
+      scores[activePlayer];
 
-  // 2. Check if players score is >= 100
-  if (scores[activePlayer] >= 20) {
-    // Finish game
-    playing = false;
-    document
-      .querySelector(`.player--${activePlayer}`)
-      .classList.add("player--winner");
-    document
-      .querySelector(`.player--${activePlayer}`)
-      .classList.remove("player--active");
-  } else {
-    // 3. Switch to the next player
-    switchPlayer();
+    // 2. Check if players score is >= 100
+    if (scores[activePlayer] >= 20) {
+      // Finish game
+      playing = false;
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add("player--winner");
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.remove("player--active");
+    } else {
+      // 3. Switch to the next player
+      switchPlayer();
+    }
   }
 });
