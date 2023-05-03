100f289eedcb209a2a9a28fb4d50d63963ca6d40
Patrick-Ehimen <osepatrick.ehimen77@gmail.com>
2023-05-03 12:35:22 +0100

    Update script.js

--- script.js
+++ script.js
@@ -14,14 +14,15 @@ const btnNew = document.querySelector(".btn--new");
 const btnRoll = document.querySelector(".btn--roll");
 const btnHold = document.querySelector(".btn--hold");
 
+let scores, currentScore, activePlayer, playing;
+
 // Starting conditions
 
 const init = function () {
-  const scores = [0, 0];
-
-  let currentScore = 0;
-  let activePlayer = 0;
-  let playing = true;
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
 
   score0El.textContent = 0;
   score1El.textContent = 0;
@@ -34,6 +35,8 @@ const init = function () {
   player0El.classList.add("player--active");
   player1El.classList.remove("player--active");
 };
+
+init();
 
 const switchPlayer = function () {
   document.getElementById(`current--${activePlayer}`).textContent = 0;
