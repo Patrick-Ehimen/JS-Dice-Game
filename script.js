9809d039a66e9330be2690a8e5590393417dff66
Patrick-Ehimen <osepatrick.ehimen77@gmail.com>
2023-05-03 11:30:06 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 "use strict";
 
 // Selecting Elements
+const player0El = document.querySelector(".player--0");
+const player1El = document.querySelector(".player--1");
+
 const score0El = document.getElementById("score--0");
 const score1El = document.getElementById("score--1");
 const current0El = document.getElementById("current--0");
@@ -41,5 +44,8 @@ btnRoll.addEventListener("click", function () {
     // current0El.textContent = currentScore; // change later
   } else {
     //switch to next player
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
   }
 });
