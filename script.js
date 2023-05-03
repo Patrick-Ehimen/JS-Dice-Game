844b141d8341c08c13943c3eec3c5b45cf517fae
Patrick-Ehimen <osepatrick.ehimen77@gmail.com>
2023-05-03 12:30:02 +0100

    Update script.js

--- script.js
+++ script.js
@@ -16,20 +16,7 @@ const btnHold = document.querySelector(".btn--hold");
 
 // Starting conditions
 
-const switchPlayer = function () {
-  document.getElementById(`current--${activePlayer}`).textContent = 0;
-  currentScore = 0;
-  activePlayer = activePlayer === 0 ? 1 : 0;
-
-  player0El.classList.toggle("player--active");
-  player1El.classList.toggle("player--active");
-};
-
 const init = function () {
-  score0El.textContent = 0;
-  score1El.textContent = 0;
-  diceEl.classList.add("hidden");
-
   const scores = [0, 0];
 
   let currentScore = 0;
@@ -41,11 +28,20 @@ const init = function () {
   current0El.textContent = 0;
   current1El.textContent = 0;
 
+  diceEl.classList.add("hidden");
   player0El.classList.remove("player--winner");
   player1El.classList.remove("player--winner");
-
   player0El.classList.add("player--active");
   player1El.classList.remove("player--active");
+};
+
+const switchPlayer = function () {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  currentScore = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+
+  player0El.classList.toggle("player--active");
+  player1El.classList.toggle("player--active");
 };
 
 // Rolling Dice functionality
